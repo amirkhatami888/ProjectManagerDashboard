80cebbafc0b54f8a2c875d9992dc54caf51c6694
amirkhatami888 <amirkhatami888@gmail.com>
2025-08-05 11:17:34 +0330

    5

--- creator_subproject/migrations/0004_remove_executive_stage_field.py
+++ creator_subproject/migrations/0004_remove_executive_stage_field.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('creator_subproject', '0003_documentfile_file_mime_type'),
+        ('creator_subproject', '0001_initial'),
     ]
 
     operations = [
