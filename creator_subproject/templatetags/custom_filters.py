c0497128637cc13db8109e5b789fe955e528e264
amirkhatami888 <amirkhatami888@gmail.com>
2025-09-06 08:27:17 +0330

    fixed

--- creator_subproject/templatetags/custom_filters.py
+++ creator_subproject/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from datetime import timedelta
 
 register = template.Library()
 
@@ -21,4 +22,29 @@ def add_class(field, css_class):
         return field.as_widget(attrs={"class": css_class})
     else:
         # Handle string value
-        return f'<span class="{css_class}">{field}</span>' 
+        return f'<span class="{css_class}">{field}</span>'
+
+@register.filter
+def timesince_timedelta(timedelta_obj):
+    """
+    Custom filter to format timedelta objects for display
+    """
+    if not timedelta_obj:
+        return "فعال"
+    
+    if not isinstance(timedelta_obj, timedelta):
+        return str(timedelta_obj)
+    
+    total_seconds = int(timedelta_obj.total_seconds())
+    
+    if total_seconds < 60:
+        return f"{total_seconds} ثانیه پیش"
+    elif total_seconds < 3600:
+        minutes = total_seconds // 60
+        return f"{minutes} دقیقه پیش"
+    elif total_seconds < 86400:
+        hours = total_seconds // 3600
+        return f"{hours} ساعت پیش"
+    else:
+        days = total_seconds // 86400
+        return f"{days} روز پیش" 
